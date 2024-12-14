16a4834111576124acba7c9e66698be2115c6b86
rpuri19 <ruchipuri19@gmail.com>
2024-12-14 04:29:52 +0100

    access data from json file

---
+++ animals_web_generator.py
@@ -0,0 +1,22 @@
+import json
+
+def load_data(file_path):
+  """ Loads a JSON file """
+  with open(file_path, "r") as handle:
+    return json.load(handle)
+
+animals_data = load_data('animals_data.json')
+number_of_records = len(animals_data)
+def animal_details():
+    for animal in animals_data:
+        animal_name = animal["name"]
+        characteristics = animal["characteristics"]
+        animal_type = characteristics.get("type")
+        animal_diet = characteristics.get("diet")
+        first_location = animal.get("locations")[0]
+        print (f"Name: {animal_name}")
+        print (f"Type: {animal_type}")
+        print (f"First Location: {first_location}")
+        print (f"Diet: {animal_diet}")
+        print("-----")
+animal_details()
