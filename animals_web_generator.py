c7efeb20bc6e9ae066d2241a48c1eb1735db3d67
rpuri19 <ruchipuri19@gmail.com>
2024-12-15 20:41:42 +0100

    data serialized using seperate function

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,25 +5,28 @@ def load_data(file_path):
   with open(file_path, "r") as handle:
     return json.load(handle)
 
-animals_data = load_data('animals_data.json')
-number_of_records = len(animals_data)
 
-def animal_details():
+def serialize_animal(animal):
+    output_data_of_animals = ""
+    animal_name = animal["name"]
+    characteristics = animal["characteristics"]
+    animal_type = characteristics.get("type")
+    animal_diet = characteristics.get("diet")
+    first_location = animal.get("locations")[0]
+    output_data_of_animals += '<li class="cards__item">\n'
+    output_data_of_animals += f'<div class="card__title">{animal_name}</div>\n'
+    output_data_of_animals += f'<p class="card__text">\n'
+    output_data_of_animals += f'<strong>Diet:</strong> {animal_diet}<br/>\n'
+    output_data_of_animals += f'<strong>Location:</strong> {first_location}<br/>\n'
+    output_data_of_animals += f'<strong>Type:</strong> {animal_type}<br/>\n'
+    output_data_of_animals += '</p>'
+    output_data_of_animals += '</li>'
+    return output_data_of_animals
+
+def animal_details(animals_data):
     output = ""
-    for animal in animals_data:
-        animal_name = animal["name"]
-        characteristics = animal["characteristics"]
-        animal_type = characteristics.get("type")
-        animal_diet = characteristics.get("diet")
-        first_location = animal.get("locations")[0]
-        output += '<li class="cards__item">\n'
-        output += f'<div class="card__title">{animal_name}</div>\n'
-        output += f'<p class="card__text">\n'
-        output += f'<strong>Diet:</strong> {animal_diet}<br/>\n'
-        output += f'<strong>Location:</strong> {first_location}<br/>\n'
-        output += f'<strong>Type:</strong> {animal_type}<br/>\n'
-        output += '</p>'
-        output += '</li>'
+    for animal_obj in animals_data:
+        output += serialize_animal(animal_obj)
     return output
 
 
@@ -39,6 +42,10 @@ def replace_data_in_html (data):
     with open('animals_output.html', 'w') as output_file:
         output_file.write(new_output)
 
-animals_list = animal_details()
-replace_data_in_html(animals_list)
+def main():
+    data_of_animals = load_data('animals_data.json')
+    animals_list = animal_details(data_of_animals)
+    replace_data_in_html(animals_list)
 
+if __name__ == "__main__":
+    main()
